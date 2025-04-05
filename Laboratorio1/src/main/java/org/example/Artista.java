b6e893fc6e38d603f8aaa6d24af3a2e7ff8b0a98
alexandrappe <00117222@uca.edu.sv>
2025-04-05 11:04:22 -0600

    Add class Disco and Class Artista :sparkles:

--- Laboratorio1/src/main/java/org/example/Artista.java
+++ Laboratorio1/src/main/java/org/example/Artista.java
@@ -1,4 +1,70 @@
 package org.example;
 
 public class Artista {
+
+    private int id;
+    private String nombre;
+    private String genero;
+    private String discoMasVendido;
+    private String ventasTotales;
+
+    public Artista(int id, String nombre, String genero, String discoMasVendido, String ventasTotales) {
+        this.id = id;
+        this.nombre = nombre;
+        this.genero = genero;
+        this.discoMasVendido = discoMasVendido;
+        this.ventasTotales = ventasTotales;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDiscoMasVendido() {
+        return discoMasVendido;
+    }
+
+    public void setDiscoMasVendido(String discoMasVendido) {
+        this.discoMasVendido = discoMasVendido;
+    }
+
+    public String getVentasTotales() {
+        return ventasTotales;
+    }
+
+    public void setVentasTotales(String ventasTotales) {
+        this.ventasTotales = ventasTotales;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", genero='" + genero + '\'' +
+                ", discoMasVendido='" + discoMasVendido + '\'' +
+                ", ventasTotales='" + ventasTotales + '\'' +
+                '}';
+    }
+
 }
--- Laboratorio1/src/main/java/org/example/Disco.java
+++ Laboratorio1/src/main/java/org/example/Disco.java
@@ -1,4 +1,47 @@
 package org.example;
 
 public class Disco {
+private Artista artista;
+private String titulo;
+private int ventasTotales;
+
+    public Disco(Artista artista, String titulo, int ventasTotales) {
+        this.artista = artista;
+        this.titulo = titulo;
+        this.ventasTotales = ventasTotales;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getVentasTotales() {
+        return ventasTotales;
+    }
+
+    public void setVentasTotales(int ventasTotales) {
+        this.ventasTotales = ventasTotales;
+    }
+
+    @Override
+    public String toString() {
+        return "Disco{" +
+                "artista=" + artista +
+                ", titulo='" + titulo + '\'' +
+                ", ventasTotales=" + ventasTotales +
+                '}';
+    }
+
 }
