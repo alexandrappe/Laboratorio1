05564b2829537343c2d32214821c2b21303dde3a
alexandrappe <00117222@uca.edu.sv>
2025-04-07 22:18:50 -0600

    Add menu and some validations :white_check_mark:

--- Laboratorio1/src/main/java/org/example/Main.java
+++ Laboratorio1/src/main/java/org/example/Main.java
@@ -1,16 +1,108 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GestionMusical tienda = new GestionMusical();
+        int opcion;
+
+        do {
+            System.out.println("\nSISTEMA DE TIENDA MUSICAL");
+            System.out.println("1. Registrar nuevo artista");
+            System.out.println("2. Registrar nuevo disco");
+
+            //Colocar opcion 3: Registrar venta de un disco
 
 
-       Artista artista1 = new Artista(1, "Taylor Swift", "Femenino", "Reputation", 10000);
-       Artista artista2 = new Artista(2, "Shawn Mendes", "Masculino", "Handwriting", 20000);
-       Artista artista3 = new Artista(3, "Paquita la del Barrio", "Femenino", "Rata Inmunda", 500);
-       Artista artista4 = new Artista(4, "Pepe el Grillo", "Masculino", "Fiesta musical", 20);
-       Artista artista5 = new Artista(5, "Bob Esponja", "Masculino", "Cacahuate", 30000);
-       Artista artista6 = new Artista (6, "Calamardo", "Masculino", "Calamardin", 1); //El mismo compro su album
+            System.out.println("4. Mostrar discos con más de X unidades vendidas");
+            //Colocar opcion 5: Artista con mas discos
+
+            System.out.println("6. Mostrar todos los artistas");
+            System.out.println("7. Mostrar todos los discos");
+
+            //Colocar opcion 8: Mostrar ventas por disco
+
+            System.out.println("9. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Esto limpia el buffer
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Nombre del artista: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("Género del artista: ");
+                    String genero = scanner.nextLine();
+                    int id = tienda.generarIdArtista();
+                    tienda.agregarArtista(new Artista(id, nombre, genero, "", 0));
+                    System.out.println("El artista fue registrado.");
+                    break;
+
+                case 2:
+                    if (!tienda.hayArtistasRegistrados()) {
+                        System.out.println("No hay artistas registrados. Primero registre un artista.");
+                        break;
+                    }
+
+                    tienda.mostrarArtistas();
+                    System.out.print("ID del artista para el disco: ");
+                    int idArtista = scanner.nextInt();
+                    scanner.nextLine();
+                    Artista artista = tienda.buscarArtistaPorId(idArtista);
+                    if (artista == null) {
+                        System.out.println("El artista no fue encontrado.");
+                        break;
+                    }
+                    System.out.print("Título del disco: ");
+                    String titulo = scanner.nextLine();
+                    System.out.print("Género del disco: ");
+                    String generoMusical = scanner.nextLine();
+                    tienda.agregarDisco(new Disco(artista, titulo, generoMusical, 0));
+                    System.out.println("El disco fue registrado.");
+                    break;
+
+                case 3:
+                    //TODO
+
+                case 4:
+                    if (!tienda.hayDiscosRegistrados()) {
+                        System.out.println("No hay discos registrados. Primero registre un disco.");
+                        break;
+                    }
+
+                    System.out.print("Ingrese el mínimo de unidades vendidas: ");
+                    int minVentas = scanner.nextInt();
+                    scanner.nextLine();
+                    tienda.mostrarDiscosPopulares(minVentas);
+                    break;
+
+
+                case 5:
+                   //TODO
+
+                case 6:
+                    tienda.mostrarArtistas();
+                    break;
+
+                case 7:
+                    tienda.mostrarDiscos();
+                    break;
+
+                case 8:
+                    //TODO
+
+                case 9:
+                    System.out.println("Gracias por usar el sistema :D");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        } while (opcion != 9);
+
+        scanner.close();
     }
 }
